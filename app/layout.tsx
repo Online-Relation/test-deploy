e08fd6dfe752ce0de327623acf57f98e823a8852
Online-Relation <mads@onlinerelation.dk>
2025-06-03 11:50:07 +0200

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import './globals.css';
-import Sidebar from '../components/Sidebar';
+import '@/globals.css';
+import Sidebar from '@/components/Sidebar';
 import { Poppins } from 'next/font/google';
 import { XpProvider } from '@/context/XpContext';
 import { CategoryProvider } from '@/context/CategoryContext';
