1c1123047fa35f064a5fb9c39e4026ce75e7d2d6
Online-Relation <mads@onlinerelation.dk>
2025-06-03 11:32:09 +0200

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
