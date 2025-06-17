2c34d5ea2c3b0a6055234e95fef2cc3e35085c3e
Online-Relation <mads@onlinerelation.dk>
2025-06-17 07:24:24 +0200

    Tilføjet .env.local

--- lib/openaiClient.ts
+++ lib/openaiClient.ts
@@ -1,8 +1,8 @@
 // /lib/openaiClient.ts
-import { OpenAI } from 'openai'
+import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: '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',
-})
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-export default openai
+export default openai;
